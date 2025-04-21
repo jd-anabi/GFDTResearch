fixing overflow errors now

hair_bundle.py
@@ -115,25 +115,26 @@ class HairBundle:
         return -1 * i_t_ca / (2 * constants.pi * z_ca * constants.elementary_charge * d_ca * r_gs)
 
     @staticmethod
-    def __hb_noise(epsilon: float, eta: float, temp: float, lam: float) -> float:
+    def __hb_noise(epsilon: float, temp: float, lam: float) -> float:
         """
         White noise for hair bundle
-        :param eta: noise control variable
-        :param tau: finite time constant
+        :param epsilon: noise control variable
+        :param temp: temperature
+        :param lam: drag coefficient
         :return: equation for the hair bundle noise
         """
-        return epsilon * np.sqrt(2 * constants.k * temp * lam) * eta
+        return epsilon * np.sqrt(2 * constants.k * temp * lam)
 
     @staticmethod
-    def __a_noise(epsilon: float, eta: float, temp: float, lam: float) -> float:
+    def __a_noise(epsilon: float, temp: float, lam: float) -> float:
         """
         Time-independent white noise for the adaptation motor
-        :param eta: noise control variable
-        :param s_max: max slipping rate
-        :param s_min: min slipping rate
+        :param epsilon: noise control variable
+        :param temp: temperature
+        :param lam: drag coefficient
         :return: equation for the adaptation motor noise
         """
-        return epsilon * np.sqrt(2 * constants.k * temp * lam) * eta
+        return epsilon * np.sqrt(2 * constants.k * temp * lam)
     # -------------------------------- ODEs --------------------------------
     @staticmethod
     def __x_hb_dot(gamma: float, n: float, f_gs: float, lambda_hb: float, k_sp: float, x_sp: float, x_hb: float) -> float:
@@ -148,7 +149,6 @@ class HairBundle:
         :param x_hb: hair bundle displacement`
         :return: time derivative of hair bundle displacement
         """
-        print("killme")
         return -1 * (gamma * n * f_gs + k_sp * (x_hb - x_sp)) / lambda_hb
 
     @staticmethod
@@ -208,8 +208,7 @@ class HairBundle:
                  k_gs_min: float, k_gs_max: float, x_c: float, temp: float, z_ca: float, d_ca: float,
                  r_m: float, r_gs: float, v_m: float, e_t_ca: float, p_t_ca: float, ca2_hb_in: float,
                  ca2_hb_ext: float, gamma: float, n: float, lambda_hb: float, lambda_a: float, k_sp: float, x_sp: float,
-                 k_es: float, x_es: float, d: float, epsilon: float, eta_hb: float, eta_a: float, omega: float, p_t_steady: bool,
-                 a: float, b: float):
+                 k_es: float, x_es: float, d: float, epsilon: float, omega: float, p_t_steady: bool, a: float, b: float):
         # parameters
         self.tau_t = tau_t # finite time constant
         self.c_min = c_min # min climbing rate
@@ -244,20 +243,15 @@ class HairBundle:
         self.x_es = x_es # extent spring displacement
         self.d = d # channel gate opening distance
         self.epsilon = epsilon # noise scale
-        self.eta_hb = eta_hb  # hair bundle diffusion constant
-        self.eta_a = eta_a  # adaptation motor diffusion constant
         self.omega = omega  # frequency of stimulus force
         self.p_t_steady = p_t_steady # binary variable dictating whether p_t should be equal to the steady-state solution
         self.a = a
         self.b = b
-        print("hello")
 
-        def driving_force(t: float, amp: float, eta: float) -> float:
+        def driving_force(t: float) -> float:
             """
             Sinusoidal stimulus force
             :param t: time
-            :param amp: amplitude
-            :param eta: amplitude for drag term
             :return: equation for a sinusoidal stimulus
             """
             return -1 * self.a * np.sin(self.omega * t) + self.b * self.omega * np.cos(self.omega * t)
@@ -268,22 +262,16 @@ class HairBundle:
         self.p_m = sym.symbols('p_m') # calcium binding probability for adaptation motor
         self.p_gs = sym.symbols('p_gs') # calcium binding probability for gating spring
         self.p_t = sym.symbols('p_t') # open channel probability
-        print("helloooooo")
 
         hb_symbols = sym.Tuple(self.x_hb, self.x_a, self.p_m, self.p_gs) # hb sympy symbols
-        print("helloooooaoda")
-        print(self.x_hb_dot)
         sdes = sym.Tuple(self.x_hb_dot, self.x_a_dot, self.p_m_dot, self.p_gs_dot)  # SDEs
-        print("hello1")
 
         # check if we don't want to use the steady state solution for the open channel probability
         if not self.p_t_steady:
             hb_symbols = hb_symbols + sym.Tuple(self.p_t)
             sdes = sdes + sym.Tuple(self.p_t_dot)
 
-        print("hi")
-        self.sde_sym_lambda_func = sym.lambdify(tuple(hb_symbols), list(sdes), cse=True)  # lambdify ode system
-        print("hi1")
+        self.sde_sym_lambda_func = sym.lambdify(tuple(hb_symbols), list(sdes), modules=["numpy"], cse=True)  # lambdify ode system
 
         def f(x: list, t: float) -> np.ndarray:
             x_sf = driving_force(t)
@@ -304,18 +292,11 @@ class HairBundle:
     # -------------------------------- Varying parameters (begin) ----------------------------------
     @property
     def k_gs(self) -> float:
-        print(self.__k_gs(self.p_gs, self.k_gs_min, self.k_gs_max).__class__)
         return sym.simplify(self.__k_gs(self.p_gs, self.k_gs_min, self.k_gs_max))
 
     @property
     def f_gs(self) -> float:
         if self.p_t_steady:
-            a = self.__f_gs(self.k_gs, self.p_t0, self.x_hb, self.x_a, self.x_c, self.d, self.gamma)
-            print(a)
-            print(a.free_symbols)
-            print(a.__class__)
-            b = sym.simplify(a)
-            print(b)
             return sym.simplify(self.__f_gs(self.k_gs, self.p_t0, self.x_hb, self.x_a, self.x_c, self.d, self.gamma))
         return sym.simplify(self.__f_gs(self.k_gs, self.p_t, self.x_hb, self.x_a, self.x_c, self.d, self.gamma))
 
@@ -345,11 +326,11 @@ class HairBundle:
 
     @property
     def hb_noise(self) -> float:
-        return sym.simplify(self.__hb_noise(self.epsilon, self.eta_hb, self.temp, self.lambda_hb))
+        return sym.simplify(self.__hb_noise(self.epsilon, self.temp, self.lambda_hb))
 
     @property
     def a_noise(self) -> float:
-        return sym.simplify(self.__a_noise(self.epsilon, self.eta_a, self.temp, self.lambda_a))
+        return sym.simplify(self.__a_noise(self.epsilon, self.temp, self.lambda_a))
     # -------------------------------- Varying parameters (end) ----------------------------------
 
     # -------------------------------- ODEs (begin) ----------------------------------

helpers.py
@@ -9,7 +9,7 @@ from numpy import ndarray, dtype
 import hair_bundle as hb
 import hair_bundle_nondimensional as hb_nd
 
-def hb_sols(t: np.ndarray, pt_steady_state: bool, s_osc: float, params: list, x0: list, nd: bool, a: float, b: float, index: int) -> tuple:
+def hb_sols(t: np.ndarray, pt_steady_state: bool, s_osc: float, params: list, x0: list, nd: bool, a: float, b: float) -> np.ndarray:
     """
     Returns sde solution for a hair bundle given a set of parameters and initial conditions
     :param t: time to solve sdes at
@@ -25,11 +25,9 @@ def hb_sols(t: np.ndarray, pt_steady_state: bool, s_osc: float, params: list, x0
     if nd:
         hb_mod = hb_nd.HairBundleNonDimensional(*params, s_osc, pt_steady_state, a, b)
     else:
-        print("here")
         hb_mod = hb.HairBundle(*params, s_osc, pt_steady_state, a, b)
-        print("here1")
     hb_sol = sdeint.itoEuler(hb_mod.f, hb_mod.g, x0, t)
-    return hb_sol, index
+    return hb_sol
 
 def auto_corr(hb_pos: np.ndarray) -> np.ndarray:
     """

main.py
@@ -8,6 +8,12 @@ import multiprocessing as mp
 import helpers
 
 if __name__ == '__main__':
+    # time and frequency arrays
+    dt = 1e-8
+    t = np.arange(0, 2, dt)
+    lims = [t[-1] - 1.5, t[-1] - 0.5]
+    freq = sp.fft.fftshift(sp.fft.fftfreq(len(t), dt))[len(t) // 2:]
+
     # whether to use the non-dimensional model or not
     user_input = input('Would you like to use the non-dimensional model (y/n): ').lower()
     if user_input in ['y', 'yes', 'true', 't', '1']:
@@ -17,7 +23,7 @@ if __name__ == '__main__':
     if nd:
         params = np.zeros(17, dtype=float)
     else:
-        params = np.zeros(35, dtype=float)
+        params = np.zeros(33, dtype=float)
 
     # read hair cell from txt file
     line = 0
@@ -64,12 +70,6 @@ if __name__ == '__main__':
     else:
         pt_steady = False
 
-    # time and frequency arrays
-    dt = 1e-5
-    t = np.arange(0, 2, dt)
-    lims = [t[-1]  - 100, t[-1] - 50]
-    freq = sp.fft.fftshift(sp.fft.fftfreq(len(t), dt))[len(t) // 2:]
-
     # solve sdes of the hair bundle
     num_trials = int(input('Number of trials less than or equal to frequency center (total number of trials is twice this values): '))
     omegas = np.zeros(2 * num_trials, dtype=float)
@@ -79,19 +79,12 @@ if __name__ == '__main__':
     for i in range(2 * num_trials):
         s_osc_curr = i * osc_freq_center / num_trials
         omegas[i] = s_osc_curr
-        args_list[i] = (t, pt_steady, s_osc_curr, params, x0, nd, amp, amp_vis, i)
+        args_list[i] = (t, pt_steady, s_osc_curr, params, x0, nd, amp, amp_vis)
     # multiprocessing
-    #with mp.Pool(mp.cpu_count()) as pool:
-    #    results = pool.starmap(helpers.hb_sols, args_list)
-    results = [helpers.hb_sols(*args_list[0])]
+    with mp.Pool(mp.cpu_count()) as pool:
+        results = pool.starmap(helpers.hb_sols, args_list)
     print(results)
-    assert False
-    results = np.array(results)
-    print(results)
-    hb_sols_trials = np.array(results[:,0])
-    #hb_sols_trials = hb_sols_trials[np.argsort(results[:,1])]
-    #hb_sols_trials = pool.starmap(helpers.hb_sols, args_list)
-    hb_sols = [[hb_sols_trials[j][:, k] for j in range(2 * num_trials)] for k in range(len(x0))]
+    hb_sols = [[results[j][:, k] for j in range(2 * num_trials)] for k in range(len(x0))]
 
     # separate driven and not driven data
     hb_pos = np.zeros(2 * num_trials, dtype=np.ndarray)