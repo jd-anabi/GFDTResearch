fixed fft scaling issues

core/helpers.py
@@ -11,14 +11,14 @@ import steady_nondimensional_model as steady_nd_model
 warnings.filterwarnings('error')
 
 if torch.cuda.is_available():
-    DEVICE = torch.device('cpu')
+    DEVICE = torch.device('cuda')
 elif torch.backends.mps.is_available():
     DEVICE = torch.device('cpu')
 else:
     DEVICE = torch.device('cpu')
 
 DTYPE = torch.float64 if DEVICE.type == 'cuda' or DEVICE.type == 'cpu' else torch.float32
-BATCH_SIZE = 64 if DEVICE.type == 'cuda' else 12
+BATCH_SIZE = 128 if DEVICE.type == 'cuda' else 12
 SDE_TYPES = ['ito', 'stratonovich']
 
 def hb_sols(t: np.ndarray, x0: list, params: list, force_params: list) -> np.ndarray:
@@ -117,17 +117,17 @@ def auto_corr(hb_pos: np.ndarray) -> np.ndarray:
     c = c[len(c) // 2:]
     return c / (len(hb_pos) * c[0])
 
-def lin_resp_ft(hb_pos: np.ndarray, sf: np.ndarray, shift: bool = True) -> np.ndarray:
+def lin_resp_ft(hb_pos: np.ndarray, sf: np.ndarray, norm: bool = True) -> np.ndarray:
     """
     Returns the linear response function (in frequency space) for the position of a hair bundle in response to a stimulus force
     :param hb_pos: the position of a hair bundle
     :param sf: the stimulus forces
-    :param shift: whether to perform a shift on the response function before returning it
+    :param norm: whether to normalize the response function
     :return: the linear response function (in frequency space)
     """
     # compute the Fourier Transform
     hb_pos_ft = sp.fft.fft2(hb_pos - np.mean(hb_pos))
     sf_pos_ft = sp.fft.fft2(sf - np.mean(sf))
-    if shift:
-        return sp.fft.fftshift(hb_pos_ft / sf_pos_ft, axes=1)
+    if norm:
+        return (hb_pos_ft / sf_pos_ft) / (len(hb_pos))
     return hb_pos_ft / sf_pos_ft

core/main.py
@@ -85,19 +85,22 @@ if __name__ == '__main__':
         hb_pos_data[i] = results[:, i, 0]
 
     # rescale
-    sf_pos, omegas = helpers.sf_pos(t, amp, 2 * np.pi * osc_freq_center)
+    sf_pos, omegas = helpers.sf_pos(t, amp, osc_freq_center)
     hb_pos, sf_pos, t = helpers.rescale(hb_pos_data, sf_pos, t, *hb_rescale_params, *hb_nd_rescale_params)
+    dt = float(t[1] - t[0])
 
     # get undriven data
     hb_pos_undriven = hb_pos[0, :]
 
     # get frequency of spontaneous oscillations
+    if n % 2 == 0:
+        upper_bound = int(n / 2)
+    else:
+        upper_bound = int((n - 1) / 2) + 1
     freqs = sp.fft.fftfreq(n, dt)
-    freqs = sp.fft.fftshift(freqs)
-    freqs = freqs[n // 2:]
-    hb_pos0_freq = sp.fft.fft(hb_pos_undriven - np.mean(hb_pos_undriven))  # fft for non-driven data
-    hb_pos0_freq = sp.fft.fftshift(hb_pos0_freq)
-    pos_mags = np.abs(hb_pos0_freq)[n // 2:]
+    freqs = freqs[:upper_bound]
+    hb_pos0_freq = sp.fft.fft(hb_pos_undriven - np.mean(hb_pos_undriven)) / len(hb_pos_undriven)  # fft for non-driven data
+    pos_mags = np.abs(hb_pos0_freq)[:upper_bound]
     peak_index = np.argmax(pos_mags)
     freqs = freqs / time_rescale # rescale from kHz -> Hz
     s_osc_freq = freqs[peak_index] # frequency of spontaneous oscillations
@@ -107,7 +110,7 @@ if __name__ == '__main__':
     sf = hb_rescale_params[4] * (hb_pos - sf_pos)
 
     # get linear response function
-    lin_resp_ft = helpers.lin_resp_ft(hb_pos, sf)
+    lin_resp_ft = helpers.lin_resp_ft(hb_pos, sf)[:upper_bound]
 
     # calculate linear response at each driving frequency
     omegas = omegas / time_rescale # rescale back to rad/s